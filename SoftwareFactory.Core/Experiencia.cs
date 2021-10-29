90b1f46c8cab23afda84fe50b65b404f4bb55d32
Liono02 <ezequiel.kippeset12@gmail.com>
2021-10-29 09:04:50 -0300

    Creadas las Propiedades de las clases

--- SoftwareFactory.Core/Experiencia.cs
+++ SoftwareFactory.Core/Experiencia.cs
@@ -4,6 +4,8 @@ namespace SoftwareFactory.Core
 {
     public class Experiencia
     {
-        
+        public Empleado Empleado { get; set; }
+        public Tecnologia tecnologia { get; set; }
+        public byte Calificacion { get; set; }
     }
 }
--- SoftwareFactory.Core/Proyecto.cs
+++ SoftwareFactory.Core/Proyecto.cs
@@ -4,6 +4,11 @@ namespace SoftwareFactory.Core
 {
     public class Proyecto
     {
-        
+        public byte IdProyecto { get; set; }
+        public Empleado Empleado { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Presupuesto { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
     }
 }
