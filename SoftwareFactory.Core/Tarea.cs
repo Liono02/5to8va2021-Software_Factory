45fafa175738cad78ec404f1a9900fe6d33a9a52
Liono02 <ezequiel.kippeset12@gmail.com>
2021-10-29 09:55:23 -0300

    Agregadas propiedades

--- SoftwareFactory.Core/Tarea.cs
+++ SoftwareFactory.Core/Tarea.cs
@@ -4,6 +4,9 @@ namespace SoftwareFactory.Core
 {
     public class Tarea
     {
-        
+        public Requerimiento Requerimiento { get; set; }
+        public Empleado Empleado { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
     }
 }
--- SoftwareFactory.Core/Tecnologia.cs
+++ SoftwareFactory.Core/Tecnologia.cs
@@ -4,6 +4,8 @@ namespace SoftwareFactory.Core
 {
     public class Tecnologia
     {
-        
+        public byte idTecnologia { get; set; }
+        public string DescripcionTecnologia { get; set; }
+        public decimal CostoBase { get; set; }
     }
 }
